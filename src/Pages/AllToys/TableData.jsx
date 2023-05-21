f1c7b2d0c899132f3a5bec00124f978f02f6e062
Abu Bakar <abubakar.hasan.cse@gmail.com>
2023-05-21 20:02:15 +0600

    data shown on table in allToy page

--- src/Pages/AllToys/TableData.jsx
+++ src/Pages/AllToys/TableData.jsx
@@ -2,14 +2,14 @@
 /* eslint-disable no-unused-vars */
 import { Link } from "react-router-dom";
 const TableData = ({item}) => {
-  const {_id} = item;
+  const { quantity, subCategoryType, price, name, sellerName, _id } = item;
   return (
     <tr className="shadow-sm my-2 py-2">
-      <td className="px-4 py-3">Start</td>
-      <td className="px-4 py-3">Start</td>
-      <td className="px-4 py-3">Start</td>
-      <td className="px-4 py-3">Start</td>
-      <td className="px-4 py-3">Start</td>
+      <td className="px-4 py-3">{sellerName}</td>
+      <td className="px-4 py-3">{name}</td>
+      <td className="px-4 py-3">{subCategoryType}</td>
+      <td className="px-4 py-3">{quantity}</td>
+      <td className="px-4 py-3"> ${price}</td>
       <td><Link to={`/allToys/${_id}`}>
              <button className="btn btn-primary">View details</button>
           </Link>
